3db73742d1db73dbe7f12e2f8624420905ac2af2
maruf-bin-salim <119032587+maruf-bin-salim@users.noreply.github.com>
2024-03-23 16:36:56 +0600

    u

--- pages/api/getIpInfo.js
+++ pages/api/getIpInfo.js
@@ -1,15 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 // should make a request to the API route and display the IP address in the browser.
-export default function handler(req, res) {
+export default async function handler(req, res) {
 
   let ip = req.headers['x-real-ip'];
 
   const forwardedFor = req.headers['x-forwarded-for'];
 
   if (!ip && forwardedFor) {
-    ip = forwardedFor?.split(",").at(0) ?? "Unknown";
+    ip = forwardedFor?.split(",").at(0) ?? null;
+    res.status(200).json({ ip: ip });
+  }
+  else {
+    // http://ip-api.com/json/103.239.252.50
+
+    const response = await fetch(`http://ip-api.com/json/${ip}`);
+    const data = await response.json();
+    res.status(200).json({ ip, data: data });
   }
 
-  res.status(200).json({ ip: ip })
 }
